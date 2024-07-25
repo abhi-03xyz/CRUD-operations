00143d028548fd83b8423ddbb377d4e75cc6baa0
Abhishek Pattanaik <abhishekpattanaik2020@gmail.com>
2024-07-25 17:42:18 +0530

    delete, edit functionallity added

--- REST/index.js
+++ REST/index.js
@@ -3,8 +3,10 @@ const app=express();
 const port=8080;
 const path=require("path");
 const {v4:uuidv4}=require("uuid");
+const methodOverride=require("method-override");
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
@@ -47,6 +49,28 @@ app.get("/posts/:id",(req,res)=>{
   // console.log(post);
   // res.send("request working");
   res.render("show.ejs",{post});
+});
+app.patch("/posts/:id",(req,res)=>{
+    let {id }=req.params;
+    console.log(id);
+    let newContent=req.body.content;
+   // console.log(newContent);
+   let post=posts.find((p)=> id===p.id);
+   post.content=newContent;
+
+   console.log(post);
+   // res.send("patch request working");
+   res.redirect("/posts");
+});
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=> id===p.id);
+    res.render("edit.ejs",{post})
+});
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    posts=posts.filter((p)=> id !== p.id);
+    res.redirect("/posts");
 });
 app.get("/",(req,res)=>{
     res.send("server working well !!");
